2d822961545c1fbb2b9ffdf3007b438833abfe3c
Tom Cassar <y49692tc@manchester.ac.uk>
2023-02-10 15:24:07 +0000

    add dockerfile for sever; host.py is set as entry point

--- server/host.py
+++ server/host.py
@@ -1,7 +1,10 @@
+"""Entry point for server"""
+
 from flask import Flask
 from flask_restful import Api
+import mysql.connector
+
+x5db = mysql.connector.connect(host="localhost", user="root", password="YOUR PASSWORD")
 
 app = Flask(__name__)
 api = Api(app)
-
- 
