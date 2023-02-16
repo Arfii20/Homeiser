2e185449ca2097eec5b58462e30c40f56221eddc
p28320aa <arefin.ahammed@student.manchester.ac.uk>
2023-02-16 14:23:52 +0000

    Managed to make date time work

--- server/shared_calendar.py
+++ server/shared_calendar.py
@@ -31,9 +31,9 @@ class SharedCalendar(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument("title_of_event", type=str, required=True, location="form",
                             help="Title of the event is required")
-        parser.add_argument("starting_time", type=list, required=True,
+        parser.add_argument("starting_time", type=str, required=True,
                             location="form", help="Starting time is required")
-        parser.add_argument("ending_time", type=list, required=True,
+        parser.add_argument("ending_time", type=str, required=True,
                             location="form", help="End time is required")
         parser.add_argument("additional_notes", type=str, required=True,
                             location="form", help="Details is required")
@@ -41,19 +41,17 @@ class SharedCalendar(Resource):
                             location="form", help="Location is required")
         args = parser.parse_args()
         title_of_event = args.get("title_of_event")
-        st = args.get("starting_time")
-        et = args.get("ending_time")
+        starting_time = args.get("starting_time")
+        ending_time = args.get("ending_time")
         additional_notes = args.get("additional_notes")
         location_of_event = args.get("location_of_event")
 
         cursor = connection.cursor()
         query = """ 
-                    INSERT INTO calender_event (title, start_time, end_time, notes, location, household_id)
+                    INSERT INTO calendar_event (title, start_time, end_time, notes, location, household_id)
                     VALUES('%s', '%s', '%s', '%s', '%s', %s);
                 """
-        data = (title_of_event, datetime.datetime(st[0], st[1], st[2], st[3], st[4], st[5]),
-                datetime.datetime(et[0], et[1], et[2], et[3], et[4], et[5]),
-                additional_notes, location_of_event, household_id)
+        data = (title_of_event, starting_time, ending_time, additional_notes, location_of_event, household_id)
         cursor.execute(query % data)
         connection.commit()
         print("Hello")
@@ -83,9 +81,22 @@ class CalendarEvent(Resource):
         Sending the event to the website when clicked on the event name from the calender
         :return: returns the whole event details
         """
+        parser = reqparse.RequestParser()
+        parser.add_argument("starting_time", type=str, required=True,
+                            location="args", help="Starting time is required")
+        parser.add_argument("ending_time", type=str, required=True,
+                            location="args", help="End time is required")
+        args = parser.parse_args()
+
+        starting_time = args.get("starting_time")
+        ending_time = args.get("ending_time")
+
         cursor = connection.cursor()
-        query = "SELECT * FROM calendar_event WHERE id = %s;"
-        cursor.execute(query % calendar_event_id)
+        query = "SELECT * FROM calendar_event WHERE id = %s " \
+                "                               AND start_time >= '%s' " \
+                "                               AND end_time <= '%s';"
+        data = (calendar_event_id, starting_time, ending_time)
+        cursor.execute(query % data)
 
         objects = []
         for x in cursor.fetchall():
@@ -102,9 +113,9 @@ class CalendarEvent(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument("title_of_event", type=str, required=True, location="form",
                             help="Title of the event is required")
-        parser.add_argument("starting_time", type=list, required=True,
+        parser.add_argument("starting_time", type=str, required=True,
                             location="form", help="Starting time is required")
-        parser.add_argument("ending_time", type=list, required=True,
+        parser.add_argument("ending_time", type=str, required=True,
                             location="form", help="End time is required")
         parser.add_argument("additional_notes", type=str, required=True,
                             location="form", help="Details is required")
@@ -174,7 +185,6 @@ class UserAttributes(Resource):
         :return: All the tagged ids
         """
         # SELECT id FROM user;
-        # # Then tag all the ids maybe?
 
 
 api.add_resource(SharedCalendar, "/shared_calendar/<int:household_id>")
--- server/test_shared_calendar.py
+++ server/test_shared_calendar.py
@@ -6,31 +6,33 @@ import datetime
 BASE = "http://127.0.0.1:5000/"
 
 # Calendar
-# response = requests.post(BASE + "shared_calendar/1", {"title_of_event": "Party",
-#                                                       "starting_time": [2023, 2, 25, 20, 00, 00],
-#                                                       "ending_time": [2023, 2, 26, 2, 00, 00],
-#                                                       "additional_notes": "Bring your own booze",
-#                                                       "location_of_event": "22 Manila House",
-#                                                       })
-# print(response.json())
+response = requests.post(BASE + "shared_calendar/1", {"title_of_event": "Party",
+                                                      "starting_time": '2023-02-19 00:00:00',
+                                                      "ending_time": '2023-02-19 20:00:00',
+                                                      "additional_notes": "Bring your own booze",
+                                                      "location_of_event": "22 Manila House",
+                                                      })
+print(response.json())
 
-response1 = requests.get(BASE + "shared_calendar/1")
-print(response1.json())
+# response1 = requests.get(BASE + "shared_calendar/2")
+# print(response1.json())
 
-response1 = requests.get(BASE + "calendar_event/1")
+response1 = requests.get(BASE + "calendar_event/2", {"starting_time": "2022-02-19 00:00:00",
+                                                     "ending_time": "2024-02-19 00:00:00"})
 print(response1.json())
 #
-# response2 = requests.put(BASE + "calendar_event/1", {"title_of_event": "Party",
-#                                                      "starting_time": [2023, 2, 25, 20, 00, 00],
-#                                                      "ending_time": [2023, 2, 26, 2, 00, 00],
-#                                                      "additional_notes": "Bring your own booze",
-#                                                      "location_of_event": "22 Manila House",
-#                                                      })
-# print(response2.json())
+response2 = requests.put(BASE + "calendar_event/2", {"title_of_event": "Party",
+                                                     "starting_time": '2023-02-19 00:00:00',
+                                                     "ending_time": '2023-02-19 20:00:00',
+                                                     "additional_notes": "Bring your own booze",
+                                                     "location_of_event": "22 Manila House",
+                                                     })
+print(response2.json())
 
 
-response1 = requests.delete(BASE + "calendar_event/1")
-print(response1.json())
 
-response1 = requests.get(BASE + "user_attributes/")
-print(response1.json())
+# response1 = requests.delete(BASE + "calendar_event/1")
+# print(response1.json())
+#
+# response1 = requests.get(BASE + "user_attributes/")
+# print(response1.json())
