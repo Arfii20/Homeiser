bc3931ad263b97474cf7372c9c61e4725698dd4e
Tom Cassar <y49692tc@manchester.ac.uk>
2023-02-21 23:07:14 +0000

    create transaction object and method to allow construction of transaction from id and cursor to db

---
+++ test_transactions/test_transaction.py
@@ -0,0 +1,40 @@
+
+from transactions.transaction import *
+from unittest import TestCase
+
+import mysql.connector
+
+class TestTransaction(TestCase):
+
+    def test_json(self):
+        """Make sure json in correct format"""
+
+        expected = json.dumps({
+            "src": "Alice",
+            "dest": "Bob",
+            "amount": 10,
+            "description": "test",
+            "due_date": '2023-02-17',
+            "paid": "false"
+        })
+
+        transaction = Transaction(0, 0, 0, 10, datetime.date(2023, 2, 17), False, "test", "Alice", "Bob")
+
+        self.assertEqual(transaction.json, expected)
+
+    def test_build_from_id(self):
+        """ Checks that we build a transaction object properly when we ask for one (given an id)
+            Also checks that a TransactionConstructionError is thrown when the database does not exist
+        """
+
+        expect = Transaction(1, 1, 2, 10, datetime.date(2023, 2, 17), False, "test", "Bob _", "Alice _")
+
+        # connect to db
+        conn = mysql.connector.connect(host="localhost", user="root", password="HALR0b0t!12", database="x5db")
+        db = conn.cursor()
+
+        got = Transaction.build_from_id(transaction_id=1, cur=db)
+
+        self.assertEqual(expect, got, "Transaction objects")
+
+
--- transactions/transaction.py
+++ transactions/transaction.py
@@ -1,7 +1,17 @@
 """Transaction Object"""
-import json
+
+from __future__ import annotations
+
 from dataclasses import dataclass
 import datetime
+import json
+from mysql.connector import cursor
+
+
+class TransactionConstructionError(Exception):
+    """Triggered when a transaction failed to build from the database"""
+
+
 @dataclass
 class Transaction:
     """Specifies a singular transaction"""
@@ -9,7 +19,7 @@ class Transaction:
     src_id: int
     dest_id: int
     amount: int
-    due: datetime.datetime
+    due: datetime.date
     paid: bool
     description: str = ""
     src_name: str = ""
@@ -24,7 +34,7 @@ class Transaction:
             "dest": <str:dest full name>,
             "amount": <int:amount>,
             "description": <str:description>
-            "due_date": <str:date string in format yyyy-mm-ddThh:mm:ss.xxxxxx> where x is a millisecond, T denotes time
+            "due_date": <str:date string in format yyyy-mm-dd>
             "paid": <str:boolean>
         }
 
@@ -38,3 +48,37 @@ class Transaction:
             "due_date": self.due.isoformat(),
             "paid": "true" if self.paid else "false"
         })
+
+    @staticmethod
+    def build_from_id(*, transaction_id: int, cur: cursor.MySQLCursor) -> Transaction:
+        """ Builds a transaction from an id in the db and a cursor to said database
+        """
+
+        cur.execute("SELECT transaction.id, u1.id, u2.id, amount, due_date, paid, description,"
+                    "CONCAT_WS(' ', u2.first_name, u2.surname), CONCAT_WS(' ', u1.first_name, u1.surname) "                
+                    "FROM transaction, pairs, user u1, user u2 "
+                    "WHERE transaction.id = %s AND pairs.id = transaction.pair_id"
+                    " AND u1.id = pairs.src AND u2.id = pairs.dest", [transaction_id])
+
+        # only one row will match an ID
+        # throw an exception if no transaction returned
+        # complain if None is returned
+
+        if (row := cur.fetchone()) is None:
+            raise TransactionConstructionError("Couldn't find transaction in the database; "
+                                               "likely due to invalid transaction ID")
+        else:
+            args: list = [element for element in row]
+
+        # args is in the form
+        # [transaction_id: int, src_id: int, dest_id: int, amount: int, due_date: str, paid: int,
+        # description: str, src_name: str, dest_name: str]
+
+        # need to convert paid from int -> bool
+        # do in place so can pass tuple directly into Transaction() instantiation
+
+        args[5] = bool(args[5])
+
+        return Transaction(*args)
+
+
