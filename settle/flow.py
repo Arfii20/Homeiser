802c94d305df58c54ad67e67efb114cf537f114a
Tom Cassar <y49692tc@manchester.ac.uk>
2023-03-07 19:01:56 +0000

    add edges and residual edges to graph

--- settle/flow.py
+++ settle/flow.py
@@ -13,13 +13,24 @@ class Vertex:
 
 @dataclass
 class Edge:
-    ...
+    target: Vertex
+    flow: int
+    capacity: int
+
+    @property
+    def unused_capacity(self) -> int:
+        return self.capacity - self.flow
+
+    @property
+    def residual(self) -> bool:
+        """(Capacity = 0) <=> edge is residual. Thus use capacity to determine if edge residual"""
+        return bool(self.capacity)
 
 
 class FlowGraph:
     """Graph with edges and residual edges. Stored as an adjacency list"""
 
-    def __init__(self, vertices: list[Vertex] = None, edges: list[Edge] = None):  # type: ignore
+    def __init__(self, vertices: list[Vertex] = None, graph: dict[Vertex, list[Edge]] = None):  # type: ignore
         """If a list of nodes is passed in, mapping from nodes to empty lists are created.
         If nodes and edges are passed in, builds the graph"""
 
@@ -27,16 +38,8 @@ class FlowGraph:
         if vertices is None:
             vertices: list[Vertex] = []  # type: ignore
 
-        # Change type of vertices to an empty list if not provided
-        if edges is None:
-            edges: list[Edge] = []  # type: ignore
-
-        # build empty adjacency graph
-        self.graph: dict[Vertex, list[Edge]] = {v: [] for v in vertices}
-
-        # add edges
-        for e in edges:
-            self.add_edge(e)
+        # If we have been provided with a graph use the graph. If not, generate an empty dict from our list of vertices
+        self.graph = graph if graph is not None else {v: [] for v in vertices}
 
     def add_vertex(self, v: Vertex):
         """Adds a vertex with no edges to the graph"""
@@ -45,11 +48,20 @@ class FlowGraph:
     def remove_vertex(self, v: Vertex):
         """Removes a vertex, and all of its incoming / outgoing edges from a graph"""
 
-
         # remove outgoing by removing
 
-    def add_edge(self, e: Edge):
-        ...
+    def add_edge(self, *,  edge: Edge, from_vertex: Vertex, add_residual=True):
+        """Adds an edge to the flow graph from a given vertex. Will also add the residual edge by default"""
+        self.graph[from_vertex].append(edge)
+
+        if add_residual:
+
+            # create the residual edge going to from_vertex from e.target
+            # flow and capacity set to 0 by def. of residual edge
+            res = Edge(from_vertex, 0, 0)
+
+            self.graph[edge.target].append(res)
+
 
     def remove_edge(self, e: Edge):
         ...
--- test/test_settle/test_flow.py
+++ test/test_settle/test_flow.py
@@ -1,15 +1,60 @@
 from unittest import TestCase
 from settle.flow import *
 
+
 class TestFlowGraph(TestCase):
+
+    def setUp(self) -> None:
+        """create a blank test graph with three nodes: {A, B, C}
+
+        A --> B --> C
+        |           ^
+        |           |
+        -------------
+
+        """
+
+        self.a = Vertex(0, 'A')
+        self.b = Vertex(1, 'B')
+        self.c = Vertex(2, 'C')
+
+        self.vertices = [self.a, self.b, self.c]
+
+        self.graph = FlowGraph(vertices=self.vertices)
+
+
+
     def test_add_vertex(self):
         g = FlowGraph()
 
         with self.subTest("Blank creation"):
             self.assertEqual(g.graph, {})
 
-        vertex = Vertex(0, 'Alice')
+        vertex = Vertex(0, "Alice")
         g.add_vertex(vertex)
 
         with self.subTest("Check vertex added as expected"):
             self.assertEqual(g.graph, {vertex: []})
+
+        # also test that the test graph had its vertices added correctly
+        with self.subTest("Check test graph"):
+            self.assertEqual(self.graph.graph, {v: [] for v in self.vertices})
+
+    def test_add_edge(self):
+        """Checks that adding an edge works"""
+
+        # add edge:  A -[0/10]-> B
+        edge = Edge(self.b, 0, 10)
+        res_edge = Edge(self.a, 0, 0)
+        self.graph.add_edge(edge=edge, from_vertex=self.a)
+
+        # check to see if the edge has been added properly
+        with self.subTest("Edge added"):
+            self.assertEqual(self.graph.graph[self.a], [edge])
+
+        # Check residual edge
+        with self.subTest("Residual edge added"):
+            self.assertEqual(self.graph.graph[self.b], [res_edge])
+
+    def test_remove_vertex(self): ...
+
