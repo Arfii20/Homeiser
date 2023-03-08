b215c3466e1091352390c042f9bc0395ff7fd546
Tom Cassar <y49692tc@manchester.ac.uk>
2023-03-08 01:48:41 +0000

    implement path building from comes from map

--- test/test_settle/test_flow_algorithms.py
+++ test/test_settle/test_flow_algorithms.py
@@ -3,25 +3,34 @@ from settle.flow_algorithms import *
 
 
 class TestMaxFlow(TestCase):
-
     def setUp(self) -> None:
         """Build the graph used in my notebook (helpful documentation)"""
 
-        self.vertices = [flow.Vertex(0, 'S')]
-        self.vertices += [flow.Vertex(i + 1, label) for i, label in enumerate(['A', 'B', 'C', 'D'])]
-        self.vertices.append(flow.Vertex(5, 'T'))
+        self.vertices = [flow.Vertex(0, "S")]
+        self.vertices += [
+            flow.Vertex(i + 1, label) for i, label in enumerate(["A", "B", "C", "D"])
+        ]
+        self.vertices.append(flow.Vertex(5, "T"))
 
         self.test_graph = flow.FlowGraph(vertices=self.vertices)
 
         s, a, b, c, d, t = self.vertices
 
-        edges = [(a, 0, 10), (b, 0, 10), (c, 0, 25), (d, 0, 15), (t, 0, 10), (a, 0, 6), (t, 0, 10)]
+        edges = [
+            (a, 0, 10),
+            (b, 0, 10),
+            (c, 0, 25),
+            (d, 0, 15),
+            (t, 0, 10),
+            (a, 0, 6),
+            (t, 0, 10),
+        ]
         srcs = [s, s, a, b, c, d, d]
 
         for edge, src in zip(edges, srcs):
             self.test_graph.add_edge(edge=flow.Edge(*edge), src=src)
 
-        self.test_graph.draw('max_flow_test')
+        self.test_graph.draw("max_flow_test")
 
     def test_edmunds_karp(self):
         ...
@@ -39,7 +48,33 @@ class TestMaxFlow(TestCase):
         ...
 
     def test__path_from_map(self):
-        ...
+
+        s, a, b, c, d, t = self.vertices
+
+        test_map: dict[flow.Vertex, flow.Vertex | None] = {s: None,
+                                                           a: d,
+                                                           b: s,
+                                                           c: None,
+                                                           d: b,
+                                                           t: None
+                                                           }
+
+        # check that an error is thrown when a map with no pointer to the sink is provided
+        with self.subTest("Invalid map - nothing pointing to sink node"), self.assertRaises(PathError):
+            MaxFlow._path_from_map(test_map, src=s, sink=t)
+
+        # fix the map
+        test_map[t] = d
+
+        with self.subTest("Build map"):
+            self.assertEqual(MaxFlow._path_from_map(test_map, src=s, sink=t), [s, b, d, t])
+
+        # break the path and make sure an error is raised
+        test_map[b] = None
+
+        with self.subTest("Path broken midway through"), self.assertRaises(PathError):
+            MaxFlow._path_from_map(test_map, src=s, sink=t)
+
 
 
 class TestSettle(TestCase):
