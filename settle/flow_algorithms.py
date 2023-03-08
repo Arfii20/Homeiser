65a7077ea3d5b9d00e190049f8bf5886fc40f4f2
Tom Cassar <y49692tc@manchester.ac.uk>
2023-03-08 01:32:44 +0000

    create the test graph; draw to dot

--- settle/flow_algorithms.py
+++ settle/flow_algorithms.py
@@ -1,15 +1,14 @@
 import settle.flow as flow
 
-
 class MaxFlow:
-
     @staticmethod
     def edmunds_karp(graph: flow.FlowGraph, src: flow.Vertex, sink: flow.Vertex) -> int:
         """Returns the max flow between src and sink nodes"""
 
-
     @staticmethod
-    def augmenting_path(graph: flow.FlowGraph, src: flow.Vertex, sink: flow.Vertex) -> list[flow.Vertex]:
+    def augmenting_path(
+        graph: flow.FlowGraph, src: flow.Vertex, sink: flow.Vertex
+    ) -> list[flow.Vertex]:
         """Returns the shortest path from src -> sink"""
 
     @staticmethod
@@ -21,9 +20,52 @@ class MaxFlow:
         """Augments the flow down a path"""
 
     @staticmethod
-    def bfs(graph: flow.FlowGraph, src: flow.Vertex, sink: flow.Vertex) -> list[flow.Vertex]:
+    def _bfs(
+        graph: flow.FlowGraph, src: flow.Vertex, sink: flow.Vertex
+    ) -> list[flow.Vertex]:
         """performs a bfs starting from a src node to a sink node; reconstructs the shortest path
         (in terms of edges traversed) from src to sink and returns it."""
+
+        # store vertices of the graph
+        vertices = graph.graph.keys()
+
+        # initialise the visited map with all nodes set to false
+        visited: dict[flow.Vertex, bool] = {v: False for v in vertices}
+
+        # initialise the came_from map with all nodes set to None
+        came_from: dict[flow.Vertex, flow.Vertex | None] = {v: None for v in vertices}
+
+        # initialise the queue; enqueue src node
+        queue: list[flow.Vertex] = [src]
+
+        while queue:
+            # dequeue into current; mark current as visited
+            current = queue.pop(0)
+            visited[current] = True
+
+            # if neighbours haven't been visited, enqueue them and mark them as coming from current
+            for neighbour in graph.neighbours(current):
+                # move on if we have already visited the neighbour
+                if visited[neighbour]:
+                    continue
+
+                # otherwise enqueue
+                queue.append(neighbour)
+
+                # and mark as coming from current node
+                came_from[neighbour] = current
+
+                # can exit early if we have just processed the sink node
+                if neighbour == sink:
+                    break
+
+        return MaxFlow._path_from_map(came_from)
+
+
+    @staticmethod
+    def _path_from_map(came_from: dict[flow.Vertex, flow.Vertex | None]) -> list[flow.Vertex]:
+        ...
+
 
 
 class Settle:
--- test/test_settle/test_flow_algorithms.py
+++ test/test_settle/test_flow_algorithms.py
@@ -1,16 +1,47 @@
 from unittest import TestCase
+from settle.flow_algorithms import *
+
 
 class TestMaxFlow(TestCase):
 
-    def test_edmunds_karp(self): ...
+    def setUp(self) -> None:
+        """Build the graph used in my notebook (helpful documentation)"""
 
-    def test_augmenting_path(self): ...
+        self.vertices = [flow.Vertex(0, 'S')]
+        self.vertices += [flow.Vertex(i + 1, label) for i, label in enumerate(['A', 'B', 'C', 'D'])]
+        self.vertices.append(flow.Vertex(5, 'T'))
 
-    def test_bottleneck(self): ...
+        self.test_graph = flow.FlowGraph(vertices=self.vertices)
 
-    def test_augment_flow(self): ...
+        s, a, b, c, d, t = self.vertices
 
-    def test_bfs(self): ...
+        edges = [(a, 0, 10), (b, 0, 10), (c, 0, 25), (d, 0, 15), (t, 0, 10), (a, 0, 6), (t, 0, 10)]
+        srcs = [s, s, a, b, c, d, d]
+
+        for edge, src in zip(edges, srcs):
+            self.test_graph.add_edge(edge=flow.Edge(*edge), src=src)
+
+        self.test_graph.draw('max_flow_test')
+
+    def test_edmunds_karp(self):
+        ...
+
+    def test_augmenting_path(self):
+        ...
+
+    def test_bottleneck(self):
+        ...
+
+    def test_augment_flow(self):
+        ...
+
+    def test__bfs(self):
+        ...
+
+    def test__path_from_map(self):
+        ...
+
 
 class TestSettle(TestCase):
-    def test_simplify_debt(self): ...
+    def test_simplify_debt(self):
+        ...
