7875fc5aeb60573034bb64f7a469aff9f6927e4b
p28320aa <arefin.ahammed@student.manchester.ac.uk>
2023-03-10 16:17:31 +0000

    Succeed getting multiple list names and input fields

--- server/shared_list/shared_list.js
+++ server/shared_list/shared_list.js
@@ -10,11 +10,54 @@ async function get_lists(household_id){
 		for (let i = 0; i < response_array.length; i++) {
 			const obj = JSON.parse(response_array[i]);
 
-			const spanElement = document.createElement("span");
-			spanElement.setAttribute("id", obj.id);
-			spanElement.innerHTML = `<u><b>List${i+1} Name: ${obj.name}</b></u>`;
-			document.getElementById('lists').appendChild(spanElement);
-			document.getElementById('lists').appendChild(document.createElement("br"));
+			// const spanElement = document.createElement("span");
+			// spanElement.setAttribute("id", obj.id);
+			// spanElement.innerHTML = `<u><b>List${i+1} Name: ${obj.name}</b></u>`;
+			// document.getElementById('lists').appendChild(spanElement);
+			// document.getElementById('lists').appendChild(document.createElement("br"));
+
+
+			// Get the parent element where you want to append the HTML
+			const parentElement = document.querySelector('div#lists');
+
+			// Create a new header element
+			const headerElement = document.createElement('header');
+			headerElement.setAttribute("id", obj.id);
+
+			// Create a new h1 element and set its text content
+			const h1Element = document.createElement('h1');
+			h1Element.textContent = obj.name;
+
+			// Create a new form element and set its attributes
+			const formElement = document.createElement('form');
+			formElement.setAttribute('id', 'new-task-form');
+
+			// Create a new input element and set its attributes
+			const inputElement = document.createElement('input');
+			inputElement.setAttribute('type', 'text');
+			inputElement.setAttribute('name', 'new-task-input');
+			inputElement.setAttribute('id', 'new-task-input');
+			inputElement.setAttribute('placeholder', 'What do you have planned?');
+
+			// Create a new input element and set its attributes
+			const submit_id = 'submit' + obj.list_id;
+			const submitElement = document.createElement('input');
+			submitElement.setAttribute('type', 'submit');
+
+			submitElement.setAttribute('id', submit_id);
+			submitElement.setAttribute('value', 'Add task');
+
+			// Append the input elements to the form element
+			formElement.appendChild(inputElement);
+			formElement.appendChild(submitElement);
+
+			// Append the h1 element and the form element to the header element
+			headerElement.appendChild(h1Element);
+			headerElement.appendChild(formElement);
+
+			// Append the header element to the parent element
+			parentElement.appendChild(headerElement);
+
 			get_list_event(obj.id);
 		}
 	}else{
