a94260dfdf09a2b0677210269bdeb71f092dac5c
Sajni Shah <sajnishah@Sajnis-MacBook-Pro.local>
2023-03-10 20:57:59 +0000

    Changing the host file to add CORS for the calender

--- server/host.py
+++ server/host.py
@@ -5,9 +5,19 @@ from flask_restful import Api  # type: ignore
 
 import server.db_handler as db_handler
 import server.endpoints as endpoints
+from flask_cors import CORS
+
+
 
 # create the app
 app = Flask(__name__)
+
+cors = [
+    CORS(app, resources={r"/shared_calender/*":{"origins": "*"}}),
+    CORS(app, resources={r"/calender_event/*": {"origins": "*"}}),
+    CORS(app, resources={r"/user_color/*": {"origins": "*"}}),
+]
+
 api = Api(app)
 endpoints.attach(api)
 app.run()
