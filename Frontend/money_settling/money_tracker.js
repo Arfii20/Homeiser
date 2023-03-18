c00c1342aa17ae387f43f6605edc8a840813dcbf
p28320aa <arefin.ahammed@student.manchester.ac.uk>
2023-03-18 13:47:46 +0000

    Make get method for each transaction

--- Frontend/money_settling/money_tracker.js
+++ Frontend/money_settling/money_tracker.js
@@ -1,4 +1,6 @@
 const BASE = "http://127.0.0.1:5000/";
+const rightContainer = document.querySelector(".container-right");
+const mainTable = document.querySelector(".main-table");
 
 async function getLedgerResources(userID){
 	var returnedData;
@@ -21,7 +23,6 @@ async function getLedgerResources(userID){
 
 	console.log(returnedData);
 
-	const mainTable = document.querySelector(".main-table");
 	mainTable.innerHTML = `<thead>
 			                <th class="header">
 			                  Source User--> Destination User
@@ -41,121 +42,161 @@ async function getLedgerResources(userID){
 	for (let i = 0; i < response_array.length; i++) {
 		const obj = await JSON.parse(response_array[i]);
 
+		console.log(obj.paid);
 		var paid;
 		if (obj.paid){
-			paid = "Yes"
+			mainTable.innerHTML += `<tbody id="data-output">
+						              	<tr id="${transaction_id}" onlick=getTransaction(event) style="text-decoration: line-through; color: gray;">
+							                <td class="table-data"> ${obj.src} --> ${obj.dest}
+							                </td>
+							                <td class="table-data">
+							                  ${obj.due_date}
+							                </td>
+							                <td class="table-data">
+							                  £${obj.amount}
+							                </td>
+							            <tr>
+						              </tbody>`;
 		}
 		else{
-			paid = "No"
+			mainTable.innerHTML +=  `<tbody id="data-output">
+						              	<tr id="${transaction_id}" onlick=getTransaction(event)>
+							                <td class="table-data"> ${obj.src} --> ${obj.dest}
+							                </td>
+							                <td class="table-data">
+							                  ${obj.due_date}
+							                </td>
+							                <td class="table-data">
+							                  £${obj.amount}
+							                </td>
+							            <tr>
+						              </tbody>`;
 		}
-
-		mainTable.innerHTML += `
-				              <tbody id="data-output">
-				                <td class="table-data"> ${obj.src} --> ${obj.dest}
-				                </td>
-				                <td class="table-data">
-				                  ${obj.due_date}
-				                </td>
-				                <td class="table-data">
-				                  £20.00
-				                </td>
-				              </tbody>
-				              `
-		// List of
-	    // {   "transaction_id": <int:transaction id>
-        //     "src_id": <int: src id>
-        //     "src_id": <int: dest id>
-        //     "src": <str:src full name>,
-        //     "dest": <str:dest full name>,
-        //     "amount": <int:amount>,
-        //     "description": <str:description>
-        //     "due_date": <str:date string in format yyyy-mm-dd>
-        //     "paid": <str:boolean>
-        // }
 	}
-
 }
 
 
-// async function getTransaction (event) {
-// 	transactionID = 234824;
+async function getTransaction (event) {
+	transactionID = event.target.id;
 
-// 	fetch(BASE + "transaction/" + transactionID)
-// 	.then(response => {
-// 	  	if (response.ok) {
-// 	    	const returnedData = response.json();
-// 	  	} else {
-// 	    	throw new Error('Error retrieving transaction.');
-// 	  	}
-// 	})
-// 	.then(data => {
-// 	  	console.log(data);
-// 	})
-// 	.catch(error => {
-// 	  	console.log(error);
-// 	});
+	fetch(BASE + "transaction/" + transactionID)
+	.then(response => {
+	  	if (response.ok) {
+	    	const returnedData = response.json();
+	  	} else {
+	    	throw new Error('Error retrieving transaction.');
+	  	}
+	})
+	.then(data => {
+	  	console.log(data);
+	})
+	.catch(error => {
+	  	console.log(error);
+	});
 
-// 	console.log(returnedData);
+	console.log(returnedData);
 
-// 		// {   "transaction_id": <int:transaction id>
-//         //     "src_id": <int: src id>
-//         //     "dest_id": <int: dest id>
-//         //     "src": <str:src full name>,
-//         //     "dest": <str:dest full name>,
-//         //     "amount": <int:amount>,
-//         //     "description": <str:description>
-//         //     "due_date": <str:date string in format yyyy-mm-dd>
-//         //     "paid": <str:boolean>
-//         // }
+	const obj = await JSON.parse(response_array[i]);
+		var paidButton;
+		if (obj.paid){
+			paidButton = "Mark as Unpaid";
+		}
+		else{
+			paidButton = "Mark as Paid";
+		}
+	rightContainer.innerHTML =  `
+								<div class="container1">
+								  <form class="text" id="transactions">
+								    <h1 class="form__title">Transactions: </h1>
+								    <div class="form__message form__message--error"></div>
+								    <div class="form__input-group">
+								      <input type="text" class="form__input" autofocus placeholder=" Full Name of Source User: ${obj.src} " readonly>
+								      <div class="form__input-error-message"></div>
+								    </div>
+								    <div class="form__input-group">
+								      <input type="password" class="form__input" autofocus placeholder=" Full Name of Destination User: ${obj.dest} " readonly>
+								      <div class="form__input-error-message"></div>
+								    </div>
+								    <div class="form__input-group">
+								      <input type="text" class="form__input" autofocus placeholder=" Amount ${obj.amount} " readonly>
+								      <div class="form__input-error-message"></div>
+								    </div>
+								    <div class="form__input-group">
+								      <input type="text" class="form__input" autofocus placeholder=" Description ${obj.description} " readonly>
+								      <div class="form__input-error-message"></div>
+								    </div>
+								    <div class="form__input-group">
+								      <input type="text" class="form__input" autofocus placeholder=" Due Date ${obj.due_date} " readonly>
+								      <div class="form__input-error-message"></div>
+								    </div>
+								    <button id="${obj.id}" class="form__button" type="submit"> ${paidButton} </button>
+								    <button id="${obj.id}" class="form__button" type="submit"> Delete </button>
+								  </form>
+								</div>
+								`;
 
+}
+
+// async function closeRightContainer(){
+// 	rightContainer.innerHTML = "";
 // }
 
+async function postTransaction(event){
 
-// async function postTransaction(event){
+		{   "transaction_id": <int:transaction id>
+            "src_id": <int: src id>
+            "dest_id": <int: dest id>
+            "src": <str:src full name>,
+            "dest": <str:dest full name>,
+            "amount": <int:amount>,
+            "description": <str:description>
+            "due_date": <str:date string in format yyyy-mm-dd>
+            "paid": <str:boolean>
+        }
 
-// 	const transaction_ID,
-// 		transaction_SrcID,
-// 		transaction_DestID,
-// 		transaction_Src,
-// 		transaction_Dest,
-// 		transaction_Amount,
-// 		transaction_Description,
-// 		transaction_DueDate,
-// 		transaction_Paid;
+	const transaction_ID,
+		transaction_SrcID,
+		transaction_DestID,
+		transaction_Src,
+		transaction_Dest,
+		transaction_Amount,
+		transaction_Description,
+		transaction_DueDate,
+		transaction_Paid;
 
 
 
 
-// 	fetch(BASE + "transaction", {
-// 	  	method: 'POST',
-// 	  	headers: {
-// 	    	'Content-Type': 'application/json'
-// 	  	},
-// 	  	body: JSON.stringify({
-// 	    	transaction_id: transaction_ID,
-// 	    	src_id: transaction_SrcID,
-// 	    	dest_id: transaction_DestID,
-// 	    	src: transaction_Src,
-// 	    	dest: transaction_Dest,
-// 	    	amount: transaction_Amount,
-// 	    	description: transaction_Description,
-// 	    	due_date: transaction_DueDate,
-// 	    	paid: transaction_Paid
-// 	  	})
-// 	})
-// 	.then(response => {
-// 	  	if (response.ok) {
-// 	    	const returnedData = response.json();
-// 	  	} else {
-// 	    	throw new Error('Request failed.');
-// 	  	}
-// 	})
-// 	.then(data => {
-// 	  	console.log(data);
-// 	})
-// 	.catch(error => {
-// 	  	console.log(error);
-// 	});
+	fetch(BASE + "transaction", {
+	  	method: 'POST',
+	  	headers: {
+	    	'Content-Type': 'application/json'
+	  	},
+	  	body: JSON.stringify({
+	    	transaction_id: transaction_ID,
+	    	src_id: transaction_SrcID,
+	    	dest_id: transaction_DestID,
+	    	src: transaction_Src,
+	    	dest: transaction_Dest,
+	    	amount: transaction_Amount,
+	    	description: transaction_Description,
+	    	due_date: transaction_DueDate,
+	    	paid: transaction_Paid
+	  	})
+	})
+	.then(response => {
+	  	if (response.ok) {
+	    	const returnedData = response.json();
+	  	} else {
+	    	throw new Error('Request failed.');
+	  	}
+	})
+	.then(data => {
+	  	console.log(data);
+	})
+	.catch(error => {
+	  	console.log(error);
+	});
 
 
 // 	console.log(returnedData);
