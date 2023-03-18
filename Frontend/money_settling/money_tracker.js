528d156c06d91489761c665bea34bec849367e2d
p28320aa <arefin.ahammed@student.manchester.ac.uk>
2023-03-18 12:14:20 +0000

    Edited the structure of html

--- Frontend/money_settling/money_tracker.js
+++ Frontend/money_settling/money_tracker.js
@@ -1,12 +1,12 @@
 const BASE = "http://127.0.0.1:5000/";
 
-async function getLedgerResources(event){
-	userID = 630;
+async function getLedgerResources(userID){
+	var returnedData;
 
 	fetch(BASE + "ledger/" + userID)
 	.then(response => {
 	  	if (response.ok) {
-	    	const returnedData = response.json();
+	    	returnedData = await response.json();
 	  	} else {
 	    	throw new Error('Error retrieving ledger.');
 	  	}
@@ -20,6 +20,37 @@ async function getLedgerResources(event){
 
 	console.log(returnedData);
 
+	const mainTable = document.querySelector(".main-table");
+	mainTable.innerHTML = `<thead>
+			                <th class="header">
+			                  Source User--> Destination User
+			                </th>
+			                <th class="header">
+			                  Due Date
+			                </th>
+			                <th class="header">
+			                  Amount
+			                </th>
+			              </thead>`
+
+	const response_array = await JSON.parse(returnedData);
+	for (let i = 0; i < response_array.length; i++) {
+		const obj = await JSON.parse(response_array[i]);
+
+		mainTable.innerHTML += `
+				              <tbody id="data-output">
+				                <!-- dat populated from js file -->
+				                <!-- example data to be removed after -->
+				                <td class="table-data"> ${obj.src} - ${obj.dest}
+				                </td>
+				                <td class="table-data">
+				                  29/03/2023
+				                </td>
+				                <td class="table-data">
+				                  £20.00
+				                </td>
+				              </tbody>
+				              `
 		// List of
 	    // {   "transaction_id": <int:transaction id>
         //     "src_id": <int: src id>
@@ -31,161 +62,162 @@ async function getLedgerResources(event){
         //     "due_date": <str:date string in format yyyy-mm-dd>
         //     "paid": <str:boolean>
         // }
+	}
 
 }
 
 
-async function getTransaction (event) {
-	transactionID = 234824;
+// async function getTransaction (event) {
+// 	transactionID = 234824;
 
-	fetch(BASE + "transaction/" + transactionID)
-	.then(response => {
-	  	if (response.ok) {
-	    	const returnedData = response.json();
-	  	} else {
-	    	throw new Error('Error retrieving transaction.');
-	  	}
-	})
-	.then(data => {
-	  	console.log(data);
-	})
-	.catch(error => {
-	  	console.log(error);
-	});
+// 	fetch(BASE + "transaction/" + transactionID)
+// 	.then(response => {
+// 	  	if (response.ok) {
+// 	    	const returnedData = response.json();
+// 	  	} else {
+// 	    	throw new Error('Error retrieving transaction.');
+// 	  	}
+// 	})
+// 	.then(data => {
+// 	  	console.log(data);
+// 	})
+// 	.catch(error => {
+// 	  	console.log(error);
+// 	});
 
-	console.log(returnedData);
+// 	console.log(returnedData);
 
-		// {   "transaction_id": <int:transaction id>
-        //     "src_id": <int: src id>
-        //     "dest_id": <int: dest id>
-        //     "src": <str:src full name>,
-        //     "dest": <str:dest full name>,
-        //     "amount": <int:amount>,
-        //     "description": <str:description>
-        //     "due_date": <str:date string in format yyyy-mm-dd>
-        //     "paid": <str:boolean>
-        // }
+// 		// {   "transaction_id": <int:transaction id>
+//         //     "src_id": <int: src id>
+//         //     "dest_id": <int: dest id>
+//         //     "src": <str:src full name>,
+//         //     "dest": <str:dest full name>,
+//         //     "amount": <int:amount>,
+//         //     "description": <str:description>
+//         //     "due_date": <str:date string in format yyyy-mm-dd>
+//         //     "paid": <str:boolean>
+//         // }
 
-}
-
-
-async function postTransaction(event){
-
-	const transaction_ID,
-		transaction_SrcID,
-		transaction_DestID,
-		transaction_Src,
-		transaction_Dest,
-		transaction_Amount,
-		transaction_Description,
-		transaction_DueDate,
-		transaction_Paid;
+// }
 
 
+// async function postTransaction(event){
 
-
-	fetch(BASE + "transaction", {
-	  	method: 'POST',
-	  	headers: {
-	    	'Content-Type': 'application/json'
-	  	},
-	  	body: JSON.stringify({
-	    	transaction_id: transaction_ID,
-	    	src_id: transaction_SrcID,
-	    	dest_id: transaction_DestID,
-	    	src: transaction_Src,
-	    	dest: transaction_Dest,
-	    	amount: transaction_Amount,
-	    	description: transaction_Description,
-	    	due_date: transaction_DueDate,
-	    	paid: transaction_Paid
-	  	})
-	})
-	.then(response => {
-	  	if (response.ok) {
-	    	const returnedData = response.json();
-	  	} else {
-	    	throw new Error('Request failed.');
-	  	}
-	})
-	.then(data => {
-	  	console.log(data);
-	})
-	.catch(error => {
-	  	console.log(error);
-	});
-
-
-	console.log(returnedData);
-
-		// {   "transaction_id": <int:transaction id>
-        //     "src_id": <int: src id>
-        //     "dest_id": <int: dest id>
-        //     "src": <str:src full name>,
-        //     "dest": <str:dest full name>,
-        //     "amount": <int:amount>,
-        //     "description": <str:description>
-        //     "due_date": <str:date string in format yyyy-mm-dd>
-        //     "paid": <str:boolean>
-        // }
-
-}
+// 	const transaction_ID,
+// 		transaction_SrcID,
+// 		transaction_DestID,
+// 		transaction_Src,
+// 		transaction_Dest,
+// 		transaction_Amount,
+// 		transaction_Description,
+// 		transaction_DueDate,
+// 		transaction_Paid;
 
 
 
-async function patchTransaction(event){
-	const transaction_Paid;
+
+// 	fetch(BASE + "transaction", {
+// 	  	method: 'POST',
+// 	  	headers: {
+// 	    	'Content-Type': 'application/json'
+// 	  	},
+// 	  	body: JSON.stringify({
+// 	    	transaction_id: transaction_ID,
+// 	    	src_id: transaction_SrcID,
+// 	    	dest_id: transaction_DestID,
+// 	    	src: transaction_Src,
+// 	    	dest: transaction_Dest,
+// 	    	amount: transaction_Amount,
+// 	    	description: transaction_Description,
+// 	    	due_date: transaction_DueDate,
+// 	    	paid: transaction_Paid
+// 	  	})
+// 	})
+// 	.then(response => {
+// 	  	if (response.ok) {
+// 	    	const returnedData = response.json();
+// 	  	} else {
+// 	    	throw new Error('Request failed.');
+// 	  	}
+// 	})
+// 	.then(data => {
+// 	  	console.log(data);
+// 	})
+// 	.catch(error => {
+// 	  	console.log(error);
+// 	});
 
 
-	fetch(BASE + "transaction/" + transactionID, {
-		method: 'PATCH',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			paid: transaction_Paid,
-		})
-	})
-	.then(response => {
-		if (response.ok) {
-			const returnedData = response;
-		} else {
-			throw new Error('Request failed.');
-		}
-	})
-	.catch(error => {
-		console.log(error);
-	});
+// 	console.log(returnedData);
 
-	console.log({message: returnedData});
+// 		// {   "transaction_id": <int:transaction id>
+//         //     "src_id": <int: src id>
+//         //     "dest_id": <int: dest id>
+//         //     "src": <str:src full name>,
+//         //     "dest": <str:dest full name>,
+//         //     "amount": <int:amount>,
+//         //     "description": <str:description>
+//         //     "due_date": <str:date string in format yyyy-mm-dd>
+//         //     "paid": <str:boolean>
+//         // }
+
+// }
 
 
-}
+
+// async function patchTransaction(event){
+// 	const transaction_Paid;
 
 
-async function deleteTransaction(event){
-	fetch(BASE + "transaction/" + transactionID, {
-	method: 'DELETE',
-	headers: {
-		'Content-Type': 'application/json'
-		}
-	})
-	.then(response => {
-		if (response.ok) {
-			const returnedData = response;
-		} else {
-			throw new Error('Request failed.');
-		}
-	})
-	.then(data => {
-		console.log(data);
-	})
-	.catch(error => {
-		console.log(error);
-	});
+// 	fetch(BASE + "transaction/" + transactionID, {
+// 		method: 'PATCH',
+// 		headers: {
+// 			'Content-Type': 'application/json'
+// 		},
+// 		body: JSON.stringify({
+// 			paid: transaction_Paid,
+// 		})
+// 	})
+// 	.then(response => {
+// 		if (response.ok) {
+// 			const returnedData = response;
+// 		} else {
+// 			throw new Error('Request failed.');
+// 		}
+// 	})
+// 	.catch(error => {
+// 		console.log(error);
+// 	});
 
-	console.log({message: returnedData});
+// 	console.log({message: returnedData});
+
+
+// }
+
+
+// async function deleteTransaction(event){
+// 	fetch(BASE + "transaction/" + transactionID, {
+// 	method: 'DELETE',
+// 	headers: {
+// 		'Content-Type': 'application/json'
+// 		}
+// 	})
+// 	.then(response => {
+// 		if (response.ok) {
+// 			const returnedData = response;
+// 		} else {
+// 			throw new Error('Request failed.');
+// 		}
+// 	})
+// 	.then(data => {
+// 		console.log(data);
+// 	})
+// 	.catch(error => {
+// 		console.log(error);
+// 	});
+
+// 	console.log({message: returnedData});
 	
-}
+// }
 
-
+getLedgerResources(630);
