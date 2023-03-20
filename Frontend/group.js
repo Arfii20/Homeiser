405ae02561a7b33e5d153f79657e52c01ec0edb0
陈小白的Mac <mac@chenxiaobaideMacBook-Pro.local>
2023-03-20 19:49:16 +0000

    database about register.js

---
+++ Frontend/group.js
@@ -0,0 +1,123 @@
+async function postGroup(event){
+	event.preventDefault();
+    console.log("sis");
+	const button = event.target;
+
+	const form = button.closest('form');
+	const group_CGElement = form.querySelector('input[placeholder*="Create Group name"]');
+	const group_JGElement = form.querySelector('input[placeholder*="Join Group name"]');
+	const group_CGPassElement = form.querySelector('input[placeholder*="Create Group password"]');
+	const group_JGPassElement = form.querySelector('input[placeholder*="Join Group password"]');
+	const group_MaxuserElement = form.querySelector('input[placeholder*="Max Users"]');
+
+	// const group_FnameID = 0;
+	// const group_LnameID = 0;
+	const group_CG = group_CGElement.value;
+	const group_JG = group_JGElement.value;
+	const group_CGPass = group_CGPassElement.value;
+    const group_JGPass = group_JGPassElement.value;
+	const group_Maxuser = group_MaxuserElement.value;
+	// const group_Paid = "false";
+	// const group_HouseId = house_id;
+
+	if (group_CG === "") {
+		setInputError(group_CGElement, 'Please enter Group Name');
+		return;
+	}
+	else{
+		clearInputError(group_CGElement);
+	}
+
+	if (group_JG === "") {
+		setInputError(group_JGElement, 'Please enter Group Name');
+		return;
+	}
+	else{
+		clearInputError(group_JGElement);
+	}
+
+	if (group_CGPass === "") {
+		setInputError(group_CGPassElement, 'Please enter Password');
+		return;
+	}
+	else if (isNaN(parseInt(group_CGPass))) {
+		setInputError(group_CGElement, 'The password is invalid');
+		return;
+	}
+	else{
+		clearInputError(group_CGElement);
+	}
+
+    if (group_JGPass === "") {
+		setInputError(group_JGPassElement, 'Please enter Password');
+		return;
+	}
+	else if (isNaN(parseInt(group_JGPass))) {
+		setInputError(group_JGElement, 'The password is invalid');
+		return;
+	}
+	else{
+		clearInputError(group_JGElement);
+	}
+
+	if (group_Maxuser === "") {
+		setInputError(group_MaxuserElement, 'Please enter max users');
+		return;
+	}
+	else{
+		clearInputError(group_MaxuserElement);
+	}
+
+	fetch(BASE + "group", {
+	  	method: 'POST',
+	  	headers: {
+	    	'Content-Type': 'application/json'
+	  	},
+	  	body: JSON.stringify({
+	    	// Fname_id: group_FnameID,
+	    	// Lname_id: group_LnameID,
+	    	fname: group_Fname,
+	    	lname: group_Lname,
+	    	// amount: parseInt(group_Amount),
+	    	confirm: group_Confirm,
+	    	password: group_Password,
+            email: group_Email,
+            color: null,
+
+	    	// paid: group_Paid,
+	    	// house_id: group_HouseId
+	  	})
+	})
+	.then(response => {
+	  	if (response.ok) {
+	    	console.log({message: "Registration successful"});
+			// rightContainer.innerHTML = "";
+			// rightContainer.style.display = "none";
+			ContinueButton.innerText = "group";
+			getLedgerResources(user_id);
+	  	} else {
+	    	throw new Error('Request failed.');
+	  	}
+	})
+	.then(data => {
+	  	console.log(data);
+	})
+	.catch(error => {
+	  	console.log(error);
+	});
+}
+
+
+function setInputError(inputElement, errorMessage, inputGroupSelector = '.form__input-group') {
+	const inputGroupElement = inputElement.closest(inputGroupSelector);
+	const errorElement = inputGroupElement.querySelector('.form__input-error-message');
+	inputGroupElement.classList.add('form__input-group--error');
+	errorElement.innerText = errorMessage;
+}
+
+function clearInputError(inputElement, inputGroupSelector = '.form__input-group') {
+	const inputGroupElement = inputElement.closest(inputGroupSelector);
+	const errorElement = inputGroupElement.querySelector('.form__input-error-message');
+	inputGroupElement.classList.remove('form__input-group--error');
+	errorElement.innerText = '';
+}
---
+++ Frontend/register.js
@@ -0,0 +1,234 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message-success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+function isValidEmail(email) {
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        //Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
+
+
+document.querySelectorAll(".form__input").forEach(inputElement => {
+    inputElement.addEventListener("blur", e => {
+        if (e.target.id === "signupEmail" && !isValidEmail(e.target.value)) {
+            setInputError(inputElement, "Wrong email address");
+        }
+    });
+
+    inputElement.addEventListener("input", e => {
+        clearInputError(inputElement);
+    });
+});
+
+document.querySelectorAll(".form__input").forEach(inputElement => {
+    inputElement.addEventListener("blur", e => {
+        if (e.target.id === "signupPassword2" && e.target.value !== document.querySelector("#signupPassword1").value) {
+            setInputError(inputElement, "Incorrect password");
+        }
+    });
+
+    inputElement.addEventListener("input", e => {
+        clearInputError(inputElement);
+    });
+});
+
+
+
+//
+async function postRegister(event){
+	event.preventDefault();
+    console.log("sis");
+	const button = event.target;
+
+	const form = button.closest('form');
+	const register_FnameElement = form.querySelector('input[placeholder*="First Name"]');
+	const register_LnameElement = form.querySelector('input[placeholder*="Last Name"]');
+	const register_EmailElement = form.querySelector('input[placeholder*="Email Address"]');
+	const register_PasswordElement = form.querySelector('input[placeholder*="Password"]');
+	const register_ConfirmElement = form.querySelector('input[placeholder*="Confirm Password"]');
+	const register_BirthElement = form.querySelector('input[placeholder*="Date of Birth"]');
+// console.log(register_BirthElement);
+	// const register_FnameID = 0;
+	// const register_LnameID = 0;
+	const register_Fname = register_FnameElement.value;
+	const register_Lname = register_LnameElement.value;
+	const register_Email = register_PasswordElement.value;
+    const register_Password = register_PasswordElement.value;
+	const register_Confirm = register_ConfirmElement.value;
+	const register_Birth = register_BirthElement.value;
+
+	// console.log(register_Birth);
+	// const register_Paid = "false";
+	// const register_HouseId = house_id;
+
+	if (register_Fname === "") {
+		setInputError(register_FnameElement, 'Please enter First Name');
+		return;
+	}
+	else{
+		clearInputError(register_FnameElement);
+	}
+
+	if (register_Lname === "") {
+		setInputError(register_LnameElement, 'Please enter Last Name');
+		return;
+	}
+	else{
+		clearInputError(register_Lname);
+	}
+
+	if (register_Birth === "") {
+		setInputError(register_BirthElement, 'Please enter birthday');
+		return;
+	}
+	else if (!isValidDate(register_Birth)){
+		setInputError(register_BirthElement, "Date must be valid and in yyyy-mm-dd format");
+		return;
+	}
+	else{
+		clearInputError(register_BirthElement);
+	}
+
+	if (register_Email === "") {
+		setInputError(register_EmailElement, 'Please enter Email');
+		return;
+	}
+	else if (isNaN(parseInt(register_Email))) {
+		setInputError(register_EmailElement, 'The email address is invalid');
+		return;
+	}
+	else{
+		clearInputError(register_EmailElement);
+	}
+
+	if (register_Password === "") {
+		setInputError(register_PasswordElement, 'Please enter password');
+		return;
+	}
+	else{
+		clearInputError(register_PasswordElement);
+	}
+
+	if (register_Confirm === "") {
+		setInputError(register_ConfirmElement, 'Please enter password again');
+		return;
+	}
+	else{
+		clearInputError(register_ConfirmElement);
+	}
+
+	if (register_Confirm === register_Password) {
+		setInputError(register_ConfirmElement, 'Passwords don\'t match');
+		return;
+	}
+	else{
+		clearInputError(register_ConfirmElement);
+	}
+
+
+
+	fetch(BASE + "register", {
+	  	method: 'POST',
+	  	headers: {
+	    	'Content-Type': 'application/json'
+	  	},
+	  	body: JSON.stringify({
+	    	// Fname_id: register_FnameID,
+	    	// Lname_id: register_LnameID,
+	    	fname: register_Fname,
+	    	lname: register_Lname,
+	    	// amount: parseInt(register_Amount),
+	    	confirm: register_Confirm,
+	    	password: register_Password,
+            email: register_Email,
+            color: null,
+
+	    	// paid: register_Paid,
+	    	// house_id: register_HouseId
+	  	})
+	})
+	.then(response => {
+	  	if (response.ok) {
+	    	console.log({message: "Registration successful"});
+			// rightContainer.innerHTML = "";
+			// rightContainer.style.display = "none";
+			ContinueButton.innerText = "Register";
+			getLedgerResources(user_id);
+	  	} else {
+	    	throw new Error('Request failed.');
+	  	}
+	})
+	.then(data => {
+	  	console.log(data);
+	})
+	.catch(error => {
+	  	console.log(error);
+	});
+}
+
+
+function setInputError(inputElement, errorMessage, inputGroupSelector = '.form__input-group') {
+	const inputGroupElement = inputElement.closest(inputGroupSelector);
+	const errorElement = inputGroupElement.querySelector('.form__input-error-message');
+	inputGroupElement.classList.add('form__input-group--error');
+	errorElement.innerText = errorMessage;
+}
+
+function clearInputError(inputElement, inputGroupSelector = '.form__input-group') {
+	const inputGroupElement = inputElement.closest(inputGroupSelector);
+	const errorElement = inputGroupElement.querySelector('.form__input-error-message');
+	inputGroupElement.classList.remove('form__input-group--error');
+	errorElement.innerText = '';
+}
+
+function isValidDate(dateString) {
+	const regex = /^\d{4}-\d{2}-\d{2}$/;
+	if (!regex.test(dateString)) {
+		return false;
+	}
+	const date = new Date(dateString);
+	if (isNaN(date.getTime())) {
+		return false;
+	}
+	return true;
+}
